c89fc78f2a8d9d8c3f03aa9fdab5aa35528e0082
Ersel Hekim <erselhekim@gmail.com>
2021-06-27 14:21:17 +0300

    post gösterme kısmı yapıldı

--- app/Http/Controllers/front/IndexController.php
+++ app/Http/Controllers/front/IndexController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\front;
 
 use App\Http\Controllers\Controller;
+use App\Models\Posts;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        return view('front.homepage');
+        $posts = Posts::orderBy('created_at', 'DESC')->paginate(1);
+        return view('front.homepage', compact('posts'));
     }
 }
---
+++ database/migrations/2021_06_27_110022_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('url');
+            $table->string('description')->nullable();
+            $table->longText('content')->nullable();
+            $table->string('image')->nullable();
+            $table->integer('user_id');
+            $table->enum('status', ['active', 'passive', 'pending'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
